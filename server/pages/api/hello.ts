d2518739ba3063e4bbb4e16eef9c7d2e57cbf445
chiamme <rito.tamata@gmail.com>
2023-02-23 12:48:05 +0900

    return Next.js server response

--- server/pages/api/hello.ts
+++ server/pages/api/hello.ts
@@ -5,9 +5,10 @@ type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await new Promise(resolve => setTimeout(() => resolve(console.log('called!')), 10000))
   res.status(200).json({ name: 'John Doe' })
 }
