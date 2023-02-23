cfd22b1137a63cdf6233b43dd97469d27726e967
chiamme <rito.tamata@gmail.com>
2023-02-23 12:48:05 +0900

    return Next.js server response

--- edge/src/index.ts
+++ edge/src/index.ts
@@ -2,16 +2,30 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-	return c.text('Hello World!')
-})
-
 app.get('/ping', (c) => {
 	return c.text('pong')
 })
 
 app.get('/json', (c) => {
 	return c.json({hoge: 'fuga'})
+})
+
+app.all('/*', async (c) => {
+	c.executionCtx.passThroughOnException()
+
+	const url = new URL(c.req.url)
+	url.port = '3000'
+
+	const blockRequest: Promise<void> = new Promise(() => fetch('http://localhost:3000/api/hello'))
+	c.executionCtx.waitUntil(blockRequest)
+
+	return await fetch(
+		url.toString(),
+		{
+			headers: c.req.headers,
+			body: c.req.body
+		}
+	)
 })
 
 export default app
