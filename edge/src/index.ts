01b3c47ebb1edc84ae2d5520f812c363012d5d37
chiamme <rito.tamata@gmail.com>
2023-02-23 16:44:32 +0900

    Add request cache sample

--- edge/src/index.ts
+++ edge/src/index.ts
@@ -13,6 +13,35 @@ app.get('/json', (c) => {
   return c.json({hoge: 'fuga'})
 })
 
+app.get('/api/cache', async (c) => {
+  const cache = caches.default
+  const cacheKey = new Request(c.req.url)
+
+  let response = await cache.match(cacheKey)
+  if (response) {
+    console.log('hit cache', c.req.url)
+    return response
+  }
+
+  response = await fetch(
+    'http://localhost:3000/api/cache',
+    {
+      cf: {
+        cacheTtl: 1000,
+        cacheEverything: true,
+      }
+    }
+  )
+
+  if (c.req.method === 'GET' && response.status === 200) {
+    response = response.clone()
+    response.headers.set('Cache-Control', 'max-age=604800')
+    c.executionCtx.waitUntil(cache.put(cacheKey, response.clone()))
+  }
+
+  return response
+})
+
 app.get('/ab/page', async (c) => {
   const abPath = c.req.cookie('ab')
   const { headers, body } = c.req
